1a3da815cc041f2ec33c7eb7d7bbdd7daaf9c6a5
DefaultUser <111709465+DefaultUser001@users.noreply.github.com>
2025-07-07 14:34:11 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+from typing import List
+
+class Settings(BaseSettings):
+    API_KEY: str
+    ALLOWED_ORIGINS: List[str] = ["*"]
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
